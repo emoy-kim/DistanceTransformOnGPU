6ea1122e3a2172b03713902cca87c1542dc8b997
jeesun <emoy.kim@gmail.com>
2023-10-06 16:45:40 +0900

    refactor: use default destructor

--- source/Renderer.cpp
+++ source/Renderer.cpp
@@ -13,11 +13,6 @@ RendererGL::RendererGL() :
    printOpenGLInformation();
 }
 
-RendererGL::~RendererGL()
-{
-   //glfwTerminate();
-}
-
 void RendererGL::printOpenGLInformation()
 {
    std::cout << "****************************************************************\n";
@@ -150,7 +145,7 @@ void RendererGL::setImageObject() const
       { 0.0f, 1.0f },
       { 0.0f, 0.0f }
    };
-   ImageObject->setObject( GL_TRIANGLES, vertices, normals, textures, "../horse.png" );
+   ImageObject->setObject( GL_TRIANGLES, vertices, normals, textures, "../test.png" );
 }
 
 void RendererGL::setDistanceObject() const
